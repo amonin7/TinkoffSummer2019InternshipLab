5ad6bf2dbcd7d65315dd28dc8a49982cdc3aaa6f
Andrey Minin <ewkjf@mail.ru>
2019-05-19 22:37:04 +0300

    Initial Commit

---
+++ Tinkoff Новости/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff Новости
+//
+//  Created by Andrey Minin on 19/05/2019.
+//  Copyright © 2019 Andrey Minin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
